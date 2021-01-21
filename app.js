16a8ade4d006c65edf786b0a47f9ac952b06897f
Michael Richardson <mor7991@yahoo.com>
2021-01-21 10:27:20 -0600

    added a reset

--- app.js
+++ app.js
@@ -1,17 +1,51 @@
 let cells = document.querySelectorAll(".row > div");
 let winningStatement = document.querySelector("h3");
-let win = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+let winningCom = [cells[0, 1, 2], cells[3, 4, 5], cells[6, 7, 8], cells[0, 3, 6], cells[1, 4, 7], cells[2, 5, 8], cells[0, 4, 8], cells[2, 4, 6]];
 let whoseTurn = true;
 let restart = ["", "", "", "", "", "", "", "", ""];
+let win = false;
 
 for (let i = 0; i < cells.length; i++) {
-    cells[i].addEventListener("click", cellClicked);
+    if(win == false){
+    cells[i].removeEventListener("click", newBoard);
+    cells[i].addEventListener("click", cellClicked, { once: true });
+    }
+    else if (win == true) {
+        cells[i].removeEventListener("click", cellClicked, { once: true });
+        cells[i].addEventListener("click", newBoard);
+    }
 }
-if (cells[1].textContent === "X") {
-    console.log("it worked!");
+
+function newBoard() {
+
+        if (win == true) {
+            cells[0].textContent = " ";
+            cells[1].textContent = " ";
+            cells[2].textContent = " ";
+            cells[3].textContent = " ";
+            cells[4].textContent = " ";
+            cells[5].textContent = " ";
+            cells[6].textContent = " ";
+            cells[7].textContent = " ";
+            cells[8].textContent = " ";
+            win = false; 
+        }
 }
 
 function cellClicked() {
+    if (win == true) {
+        cells[0].textContent = " ";
+        cells[1].textContent = " ";
+        cells[2].textContent = " ";
+        cells[3].textContent = " ";
+        cells[4].textContent = " ";
+        cells[5].textContent = " ";
+        cells[6].textContent = " ";
+        cells[7].textContent = " ";
+        cells[8].textContent = " ";
+        win = false; 
+    }
+
     if (whoseTurn == true) {
         event.target.textContent = "X";
         winningStatement.textContent = "O's turn";
@@ -22,71 +56,87 @@ function cellClicked() {
         winningStatement.textContent = "X's turn";
         whoseTurn = true;
     }
-    for (j = 0; j < win.length; j++) {
+    for (let j = 0; j < winningCom.length; j++) {
         if (cells[0].textContent === "X" && cells[1].textContent === "X" && cells[2].textContent === "X") {
             console.log("it worked!");
-            cells.textContent = restart;
+            win = true;
             break;
         } else if (cells[3].textContent === "X" && cells[4].textContent === "X" && cells[5].textContent === "X") {
             console.log("it worked!");
+            win = true;
             break;
         } else if (cells[6].textContent === "X" && cells[7].textContent === "X" && cells[8].textContent === "X") {
             console.log("it worked!");
+            win = true;
             break;
         }
         else if (cells[0].textContent === "X" && cells[3].textContent === "X" && cells[6].textContent === "X") {
             console.log("it worked!");
+            win = true;
             break;
         }
         else if (cells[1].textContent === "X" && cells[4].textContent === "X" && cells[7].textContent === "X") {
             console.log("it worked!");
+            win = true;
             break;
         }
         else if (cells[2].textContent === "X" && cells[5].textContent === "X" && cells[8].textContent === "X") {
             console.log("it worked!");
+            win = true;
             break;
         }
         else if (cells[0].textContent === "X" && cells[4].textContent === "X" && cells[8].textContent === "X") {
             console.log("it worked!");
+            win = true;
             break;
         }
         else if (cells[2].textContent === "X" && cells[4].textContent === "X" && cells[6].textContent === "X") {
             console.log("it worked!");
+            win = true;
             break;
         }
         else if (cells[0].textContent === "O" && cells[1].textContent === "O" && cells[2].textContent === "O") {
             console.log("it worked!");
+            win = true;
             break;
         } else if (cells[3].textContent === "O" && cells[4].textContent === "O" && cells[5].textContent === "O") {
             console.log("it worked!");
+            win = true;
             break;
         } else if (cells[6].textContent === "O" && cells[7].textContent === "O" && cells[8].textContent === "O") {
             console.log("it worked!");
+            win = true;
             break;
         }
         else if (cells[0].textContent === "O" && cells[3].textContent === "O" && cells[6].textContent === "O") {
             console.log("it worked!");
+            win = true;
             break;
         }
         else if (cells[1].textContent === "O" && cells[4].textContent === "O" && cells[7].textContent === "O") {
             console.log("it worked!");
+            win = true;
             break;
         }
         else if (cells[2].textContent === "O" && cells[5].textContent === "O" && cells[8].textContent === "O") {
             console.log("it worked!");
+            win = true;
             break;
         }
         else if (cells[0].textContent === "O" && cells[4].textContent === "O" && cells[8].textContent === "O") {
             console.log("it worked!");
+            win = true;
             break;
         }
         else if (cells[2].textContent === "O" && cells[4].textContent === "O" && cells[6].textContent === "O") {
             console.log("it worked!");
+            win = true;
             break;
         }
         else {
             console.log("nope.");
         }
+
     }
 
 }
