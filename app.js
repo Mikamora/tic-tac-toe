eec6089795bb13dfd38749794204cb9bfcc404b7
Michael Richardson <mor7991@yahoo.com>
2021-01-21 18:12:10 -0600

    commented out useless code

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ let cells = document.querySelectorAll(".row > div");
 let board = document.querySelector("#board");
 let button = document.querySelector("button");
 let winningStatement = document.querySelector("h3");
-let winningCom = [cells[0, 1, 2], cells[3, 4, 5], cells[6, 7, 8], cells[0, 3, 6], cells[1, 4, 7], cells[2, 5, 8], cells[0, 4, 8], cells[2, 4, 6]];
+// let winningCom = [cells[0, 1, 2], cells[3, 4, 5], cells[6, 7, 8], cells[0, 3, 6], cells[1, 4, 7], cells[2, 5, 8], cells[0, 4, 8], cells[2, 4, 6]];
 let whoseTurn = true;
-let restart = ["", "", "", "", "", "", "", "", ""];
-let win = false;
+// let restart = ["", "", "", "", "", "", "", "", ""];
+// let win = false;
 board.addEventListener("click", checkWin);
 button.addEventListener("click", startOver);
 
@@ -35,73 +35,73 @@ function checkWin() {
     }
     if (cells[0].textContent === "X" && cells[1].textContent === "X" && cells[2].textContent === "X") {
         winningStatement.textContent = "X Wins!";
-        win = true;
+        // win = true;
         board.addEventListener("click", startOver);
 
     } else if (cells[3].textContent === "X" && cells[4].textContent === "X" && cells[5].textContent === "X") {
         winningStatement.textContent = "X Wins!";
-        win = true;
+        // win = true;
         board.addEventListener("click", startOver);
     } else if (cells[6].textContent === "X" && cells[7].textContent === "X" && cells[8].textContent === "X") {
         winningStatement.textContent = "X Wins!";
-        win = true;
+        // win = true;
         board.addEventListener("click", startOver);
     } else if (cells[0].textContent === "X" && cells[3].textContent === "X" && cells[6].textContent === "X") {
         winningStatement.textContent = "X Wins!";
-        win = true;
+        // win = true;
         board.addEventListener("click", startOver);
     } else if (cells[1].textContent === "X" && cells[4].textContent === "X" && cells[7].textContent === "X") {
         winningStatement.textContent = "X Wins!";
-        win = true;
+        // win = true;
         board.addEventListener("click", startOver);
     } else if (cells[2].textContent === "X" && cells[5].textContent === "X" && cells[8].textContent === "X") {
         winningStatement.textContent = "X Wins!";
-        win = true;
+        // win = true;
         board.addEventListener("click", startOver);
     } else if (cells[0].textContent === "X" && cells[4].textContent === "X" && cells[8].textContent === "X") {
         winningStatement.textContent = "X Wins!";
-        win = true;
+        // win = true;
         board.addEventListener("click", startOver);
     } else if (cells[2].textContent === "X" && cells[4].textContent === "X" && cells[6].textContent === "X") {
         winningStatement.textContent = "X Wins!";
-        win = true;
+        // win = true;
         board.addEventListener("click", startOver);
     } else if (cells[0].textContent === "O" && cells[1].textContent === "O" && cells[2].textContent === "O") {
         winningStatement.textContent = "O Wins!";
-        win = true;
+        // win = true;
         board.addEventListener("click", startOver);
     } else if (cells[3].textContent === "O" && cells[4].textContent === "O" && cells[5].textContent === "O") {
         winningStatement.textContent = "O Wins!";
-        win = true;
+        // win = true;
         board.addEventListener("click", startOver);
     } else if (cells[6].textContent === "O" && cells[7].textContent === "O" && cells[8].textContent === "O") {
         winningStatement.textContent = "O Wins!";
-        win = true;
+        // win = true;
         board.addEventListener("click", startOver);
     } else if (cells[0].textContent === "O" && cells[3].textContent === "O" && cells[6].textContent === "O") {
         winningStatement.textContent = "O Wins!";
-        win = true;
+        // win = true;
         board.addEventListener("click", startOver);
     } else if (cells[1].textContent === "O" && cells[4].textContent === "O" && cells[7].textContent === "O") {
         winningStatement.textContent = "O Wins!";
-        win = true;
+        // win = true;
         board.addEventListener("click", startOver);
     } else if (cells[2].textContent === "O" && cells[5].textContent === "O" && cells[8].textContent === "O") {
         winningStatement.textContent = "O Wins!";
-        win = true;
+        // win = true;
         board.addEventListener("click", startOver);
     } else if (cells[0].textContent === "O" && cells[4].textContent === "O" && cells[8].textContent === "O") {
         winningStatement.textContent = "O Wins!";
-        win = true;
+        // win = true;
         board.addEventListener("click", startOver);
     } else if (cells[2].textContent === "O" && cells[4].textContent === "O" && cells[6].textContent === "O") {
         winningStatement.textContent = "O Wins!";
-        win = true;
+        // win = true;
         board.addEventListener("click", startOver);
     } else {
         if (cells[0].textContent != "" && cells[1].textContent != "" && cells[2].textContent != "" && cells[3].textContent != "" && cells[4].textContent != "" && cells[5].textContent != "" && cells[6].textContent != "" && cells[7].textContent != "" && cells[8].textContent != ""){
             winningStatement.textContent = "Draw!";
-            win = true;
+            // win = true;
             board.addEventListener("click", startOver);
             
         }
